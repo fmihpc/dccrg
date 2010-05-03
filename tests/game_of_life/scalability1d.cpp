b4431b1c7e45c9744ba242763823b90bcf63b6e9
Ilja Honkonen <ilja.honkonen@fmi.fi>
2010-05-03 12:51:37 +0300

    Add a 1d game of life scalability test

---
+++ tests/game_of_life/scalability1d.cpp
@@ -0,0 +1,171 @@
+/*
+Tests the scalability of the grid in 2 D
+*/
+
+#include "algorithm"
+#include "boost/mpi.hpp"
+#include "boost/unordered_set.hpp"
+#include "cstdlib"
+#include "ctime"
+#include "../../dccrg.hpp"
+#include "fstream"
+#include "iostream"
+#include "zoltan.h"
+
+
+struct game_of_life_cell {
+
+	template<typename Archiver> void serialize(Archiver& ar, const unsigned int /*version*/) {
+		ar & is_alive;
+	}
+
+	bool is_alive;
+	unsigned int live_neighbour_count;
+};
+
+
+using namespace std;
+using namespace boost::mpi;
+
+int main(int argc, char* argv[])
+{
+	environment env(argc, argv);
+	communicator comm;
+
+	clock_t before, after, total = 0;
+
+	float zoltan_version;
+	if (Zoltan_Initialize(argc, argv, &zoltan_version) != ZOLTAN_OK) {
+	    cout << "Zoltan_Initialize failed" << endl;
+	    exit(EXIT_FAILURE);
+	}
+	if (comm.rank() == 0) {
+		cout << "Using Zoltan version " << zoltan_version << endl;
+	}
+
+
+	#define STARTING_CORNER 0.0
+	#define GRID_SIZE 1000000	// in unrefined cells
+	#define CELL_SIZE (1.0 / GRID_SIZE)
+	#define STENCIL_SIZE 1
+	#define MAX_REFINEMENT_LEVEL 0
+	dccrg<game_of_life_cell> game_grid(comm, "RCB", STARTING_CORNER, STARTING_CORNER, STARTING_CORNER, CELL_SIZE, GRID_SIZE, 1, 1, STENCIL_SIZE, MAX_REFINEMENT_LEVEL);
+	if (comm.rank() == 0) {
+		cout << "Maximum refinement level of the grid: " << game_grid.get_max_refinement_level() << endl;
+		cout << "Number of cells: " << GRID_SIZE * 1 * 1 << endl << endl;
+	}
+
+	game_grid.balance_load();
+	comm.barrier();
+
+	vector<uint64_t> cells_with_local_neighbours = game_grid.get_cells_with_local_neighbours();
+	vector<uint64_t> cells_with_remote_neighbour = game_grid.get_cells_with_remote_neighbour();
+	cout << "Process " << comm.rank() << ": number of cells with local neighbours: " << cells_with_local_neighbours.size() << ", number of cells with a remote neighbour: " << cells_with_remote_neighbour.size() << endl;
+
+	// initialize the game with random cells alive
+	srand(time(NULL));
+	for (vector<uint64_t>::const_iterator cell = cells_with_local_neighbours.begin(); cell != cells_with_local_neighbours.end(); cell++) {
+
+		game_of_life_cell* cell_data = game_grid[*cell];
+		cell_data->live_neighbour_count = 0;
+
+		if (double(rand()) / RAND_MAX <= 0.2) {
+			cell_data->is_alive = true;
+		} else {
+			cell_data->is_alive = false;
+		}
+	}
+	for (vector<uint64_t>::const_iterator cell = cells_with_remote_neighbour.begin(); cell != cells_with_remote_neighbour.end(); cell++) {
+
+		game_of_life_cell* cell_data = game_grid[*cell];
+		cell_data->live_neighbour_count = 0;
+
+		if (double(rand()) / RAND_MAX <= 0.2) {
+			cell_data->is_alive = true;
+		} else {
+			cell_data->is_alive = false;
+		}
+	}
+
+	if (comm.rank() == 0) {
+		cout << "step: ";
+	}
+
+	#define TIME_STEPS 100
+	before = clock();
+	for (int step = 0; step < TIME_STEPS; step++) {
+
+		if (comm.rank() == 0) {
+			cout << step << " ";
+			cout.flush();
+		}
+
+		game_grid.start_remote_neighbour_data_update();
+		// get the neighbour counts of every cell, starting with the cells whose neighbour data doesn't come from other processes
+		for (vector<uint64_t>::const_iterator cell = cells_with_local_neighbours.begin(); cell != cells_with_local_neighbours.end(); cell++) {
+
+			game_of_life_cell* cell_data = game_grid[*cell];
+			cell_data->live_neighbour_count = 0;
+
+			const vector<uint64_t>* neighbours = game_grid.get_neighbours(*cell);
+			for (vector<uint64_t>::const_iterator neighbour = neighbours->begin(); neighbour != neighbours->end(); neighbour++) {
+
+				game_of_life_cell* neighbour_data = game_grid[*neighbour];
+				if (neighbour_data->is_alive) {
+					cell_data->live_neighbour_count++;
+				}
+			}
+		}
+
+		// wait for neighbour data updates to finish and go through the rest of the cells
+		game_grid.wait_neighbour_data_update();
+		for (vector<uint64_t>::const_iterator cell = cells_with_remote_neighbour.begin(); cell != cells_with_remote_neighbour.end(); cell++) {
+
+			game_of_life_cell* cell_data = game_grid[*cell];
+			cell_data->live_neighbour_count = 0;
+
+			const vector<uint64_t>* neighbours = game_grid.get_neighbours(*cell);
+			for (vector<uint64_t>::const_iterator neighbour = neighbours->begin(); neighbour != neighbours->end(); neighbour++) {
+
+				game_of_life_cell* neighbour_data = game_grid[*neighbour];
+				if (neighbour_data->is_alive) {
+					cell_data->live_neighbour_count++;
+				}
+			}
+		}
+
+		// calculate the next turn
+		for (vector<uint64_t>::const_iterator cell = cells_with_local_neighbours.begin(); cell != cells_with_local_neighbours.end(); cell++) {
+
+			game_of_life_cell* cell_data = game_grid[*cell];
+
+			if (cell_data->live_neighbour_count == 3) {
+				cell_data->is_alive = true;
+			} else if (cell_data->live_neighbour_count != 2) {
+				cell_data->is_alive = false;
+			}
+		}
+		for (vector<uint64_t>::const_iterator cell = cells_with_remote_neighbour.begin(); cell != cells_with_remote_neighbour.end(); cell++) {
+
+			game_of_life_cell* cell_data = game_grid[*cell];
+
+			if (cell_data->live_neighbour_count == 3) {
+				cell_data->is_alive = true;
+			} else if (cell_data->live_neighbour_count != 2) {
+				cell_data->is_alive = false;
+			}
+		}
+	}
+	after = clock();
+	total += after - before;
+	if (comm.rank() == 0) {
+		cout << endl;
+		cout.flush();
+	}
+	comm.barrier();
+
+	int number_of_cells = cells_with_local_neighbours.size() + cells_with_remote_neighbour.size();
+	cout << "Process " << comm.rank() << ": " << number_of_cells * TIME_STEPS << " cells processed at the speed of " << double(number_of_cells * TIME_STEPS) * CLOCKS_PER_SEC / total << " cells / second"<< endl;
+
+	return EXIT_SUCCESS;
+}
