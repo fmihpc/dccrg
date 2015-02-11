dc80c07654a93430d206c83ea06ea0fa4de51458
Ilja Honkonen <ilja.honkonen@fmi.fi>
2015-02-10 19:48:38 -0500

    Add a version of get_cell_mpi_datatype() that returns a human-readable error message.

--- dccrg_get_cell_datatype.hpp
+++ dccrg_get_cell_datatype.hpp
@@ -21,13 +21,15 @@ along with dccrg. If not, see <http://www.gnu.org/licenses/>.
 #define DCCRG_GET_CELL_DATATYPE_HPP
 
 
+#include "cstdint"
+#include "tuple"
+#include "type_traits"
+
+#include "mpi.h"
+
 #include "boost/function_types/property_tags.hpp"
 #include "boost/mpl/vector.hpp"
 #include "boost/tti/has_member_function.hpp"
-#include "tuple"
-#include "type_traits"
-#include "cstdint"
-#include "mpi.h"
 
 
 namespace dccrg {
@@ -207,6 +209,67 @@ template<
 	const int /*neighborhood_id*/
 ) {
 	return cell.get_mpi_datatype();
+}
+
+
+/*!
+Returns a human-readable error message if cell type doesn't have suitable get_mpi_datatype().
+*/
+template<
+	class Cell_T
+> typename std::enable_if<
+	not has_member_function_get_mpi_datatype<
+		Cell_T,
+		std::tuple<void*, int, MPI_Datatype>,
+		boost::mpl::vector<
+			const uint64_t,
+			const int,
+			const int,
+			const bool,
+			const int
+		>,
+		boost::function_types::const_qualified
+	>::value
+	and not has_member_function_get_mpi_datatype<
+		Cell_T,
+		std::tuple<void*, int, MPI_Datatype>,
+		boost::mpl::vector<
+			const uint64_t,
+			const int,
+			const int,
+			const bool,
+			const int
+		>
+	>::value
+	and not has_member_function_get_mpi_datatype<
+		Cell_T,
+		std::tuple<void*, int, MPI_Datatype>,
+		boost::mpl::vector<>,
+		boost::function_types::const_qualified
+	>::value
+	and not has_member_function_get_mpi_datatype<
+		Cell_T,
+		std::tuple<void*, int, MPI_Datatype>,
+		boost::mpl::vector<>
+	>::value,
+	std::tuple<
+		void*,
+		int,
+		MPI_Datatype
+	>
+>::type get_cell_mpi_datatype(
+	Cell_T& cell,
+	const uint64_t /*cell_id*/,
+	const int /*sender*/,
+	const int /*receiver*/,
+	const bool /*receiving*/,
+	const int /*neighborhood_id*/
+) {
+	static_assert(
+		not std::is_same<Cell_T, Cell_T>::value,
+		"No suitable get_mpi_datatype() member function found in cell type given to dccrg"
+	);
+	return std::make_tuple(nullptr, -1, MPI_DATATYPE_NULL);
 }
 
 
