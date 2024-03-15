cf03b7b162694a726ad3d200d38100b89b224f86
lkotipal <leo.kotipalo@helsinki.fi>
2024-03-15 13:45:33 +0200

    Test user neighborhood consistency

--- UnitTests/grid.cpp
+++ UnitTests/grid.cpp
@@ -17,25 +17,73 @@ int main(int argc, char* argv[])
 	return ret;
 }
 
-class SimpleGrid : public testing::Test {
+enum class GridType {
+	simple,
+	vlasovian,
+	magnetospheric
+};
+
+class GridTest : public testing::TestWithParam<GridType> {
 	protected:
-	SimpleGrid()
+	void SetUp() override
 	{
-		// Defaults should be fine for everything
-		grid.set_initial_length({4, 4, 4}).initialize(MPI_COMM_WORLD).set_load_balancing_method("RANDOM").balance_load();
+		typedef dccrg::Types<3>::neighborhood_item_t neigh_t;
+		std::vector<neigh_t> neighborhood;
+
+		grid.set_initial_length({10, 10, 10}).set_neighborhood_length(stencil_width).initialize(MPI_COMM_WORLD);
+		switch (GetParam()) {
+			case GridType::simple:
+				break; // Defaults should be fine for everything
+			case GridType::vlasovian:
+				// Extended sysboundaries
+				for (int x = -stencil_width; x <= stencil_width; ++x) {
+					for (int y = -stencil_width; y <= stencil_width; ++y) {
+						for (int z = -stencil_width; z <= stencil_width; ++z) {
+							if (x || y || z) {
+								neighborhood.push_back({x, y, z});
+							}
+						}
+					}
+				}
+				grid.add_neighborhood(neighborhoods++, neighborhood);
+
+				// Vlasov solver
+				for (int d = -stencil_width; d <= stencil_width; ++d) {
+					if (d) {
+						neighborhood.push_back({d, 0, 0});
+						neighborhood.push_back({0, d, 0});
+						neighborhood.push_back({0, 0, d});
+					}
+				}
+				grid.add_neighborhood(neighborhoods++, neighborhood);
+
+				break;
+			default:
+				FAIL() << "Grid type not implemented!";
+				break;
+		}
+
+		grid.set_load_balancing_method("RANDOM").balance_load();
 
 		// Simple data we can check
 		for (auto cell : grid.get_cells()) {
 			*grid[cell] = cell;
 		}
-
 		grid.update_copies_of_remote_neighbors();
+		for (int neighborhood = 0; neighborhood < neighborhoods; ++neighborhood) {
+			grid.update_copies_of_remote_neighbors(neighborhood);
+		}
 	}
 
 	dccrg::Dccrg<uint64_t, dccrg::Cartesian_Geometry> grid;
+	int neighborhoods {0};
+	const int stencil_width {3};
 };
 
-TEST_F(SimpleGrid, contents)
+INSTANTIATE_TEST_SUITE_P(Simple, GridTest, testing::Values(GridType::simple));
+INSTANTIATE_TEST_SUITE_P(Vlasovian, GridTest, testing::Values(GridType::vlasovian));
+
+TEST_P(GridTest, contents)
 {
 	for (auto cell : grid.get_cells()) {
 		EXPECT_NE_MPI(grid[cell], nullptr);
@@ -43,7 +91,7 @@ TEST_F(SimpleGrid, contents)
 	}
 }
 
-TEST_F(SimpleGrid, contents_after_loadbalance)
+TEST_P(GridTest, contents_after_loadbalance)
 {
 	grid.balance_load();
 	for (auto cell : grid.get_cells()) {
@@ -52,8 +100,7 @@ TEST_F(SimpleGrid, contents_after_loadbalance)
 	}
 }
 
-// TODO: currently only checks consistency between local cells
-TEST_F(SimpleGrid, consistent_neighbors)
+TEST_P(GridTest, consistent_neighbors)
 {
 	auto cells = grid.get_cells();
 	for (auto cell : grid.get_cells()) {
@@ -96,9 +143,47 @@ TEST_F(SimpleGrid, consistent_neighbors)
 	}
 }
 
+// TODO: cannot test consistency of remote neighbors without dccrg changes
+TEST_P(GridTest, consistent_user_neighbors)
+{
+	auto cells = grid.get_cells();
+	for (auto cell : grid.get_cells()) {
+		for (int neighborhood = 0; neighborhood < neighborhoods; ++neighborhood) {
+			auto* my_neighbors_of {grid.get_neighbors_of(cell, neighborhood)};
+			EXPECT_NE_MPI(my_neighbors_of, nullptr);
+
+			for (auto [neighbor, dir] : *my_neighbors_of) {
+				if (neighbor != dccrg::error_cell) {
+					EXPECT_NE_MPI(grid[neighbor], nullptr);
+					std::vector<std::pair<uint64_t, std::array<int, 4>>> other_neighbors_to;
+					if (std::find(cells.begin(), cells.end(), neighbor) != cells.end()) {
+						auto* p {grid.get_neighbors_to(neighbor, neighborhood)};
+						EXPECT_NE_MPI(p, nullptr);
+						other_neighbors_to = *p;
+						EXPECT_NE_MPI(std::find_if(other_neighbors_to.begin(), other_neighbors_to.end(), [&cell](const std::pair<const uint64_t, std::array<int, 4>> pair){return pair.first == cell;}), other_neighbors_to.end());
+					}
+				}
+			}
+
+			auto* my_neighbors_to {grid.get_neighbors_to(cell, neighborhood)};
+			EXPECT_NE_MPI(my_neighbors_to, nullptr);
+
+			for (auto [neighbor, dir] : *my_neighbors_to) {
+				std::vector<std::pair<uint64_t, std::array<int, 4>>> other_neighbors_of;
+				if (std::find(cells.begin(), cells.end(), neighbor) != cells.end()) {
+					auto* p {grid.get_neighbors_of(neighbor, neighborhood)};
+					EXPECT_NE_MPI(p, nullptr);
+					other_neighbors_of = *p;
+					EXPECT_NE_MPI(std::find_if(other_neighbors_of.begin(), other_neighbors_of.end(), [&cell](const std::pair<const uint64_t, std::array<int, 4>> pair){return pair.first == cell;}), other_neighbors_of.end());
+				}
+			}
+		}
+	}
+}
+
 // TODO test proper copies and frees of dccrg.comm
 // Right now this can't be done because the getter is not a getter
-TEST_F(SimpleGrid, copy)
+TEST_P(GridTest, copy)
 {
 	auto other_grid = grid;
 
